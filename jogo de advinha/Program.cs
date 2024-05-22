af39b0121d7d275ce75465a3c661820ed462b8cd
Binato <Binato@DESKTOP-QQO559V>
2024-05-22 20:50:18 -0300

    Adicionar arquivos de projeto.

---
+++ jogo de advinha/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo_de_advinha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "amarelo";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Faça uma tentativa: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else 
+                {
+                    outOfGuesses = true;
+                }
+
+                
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("Voce Perdeu !");
+            }
+            else
+            {
+                Console.Write("Voce Venceu !");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
